4b7a45cbe1b92bb44720c070041f34123ee941c1
Alex SHD <alex@shdlabs.com>
2016-06-05 00:35:31 +0300

    proper bash lint

--- shd-base-setup.sh
+++ shd-base-setup.sh
@@ -1,35 +1,40 @@
 #!/bin/sh
 
 ubuntu_update() {
-  sudo apt-get update -qq
+    sudo apt-get update -qq
+    return 0
 }
 
 base_packages() {
-  for package_name in tmux vim git zsh mlocate httpie curl unzip
-  do
-    sudo apt-get install $package_name -y -qq
-  done
-  return 0
+    for package_name in tmux vim git zsh mlocate httpie curl unzip
+    do
+        sudo apt-get install $package_name -y -qq
+    done
+    return 0
 }
 
 install_chef() {
-  curl -L https://www.chef.io/chef/install.sh | sudo bash
-  return 0
+    curl -L https://www.chef.io/chef/install.sh | sudo bash
+    return 0
 }
 
 setup_vim() {
-  # clone Vundle to users home dir
-  git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-  # clone personal .vimrc
-  curl https://gist.githubusercontent.com/alexshd/756459d0e840c02d47ee/raw/d6235761fb360bad93db6017f3fedbd65a871ab1/.vimrc > ~/.vimrc
-  vim +PluginInstall +qall  2>&1
+    # clone Vundle to users home dir
+    git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    # clone personal .vimrc
+    curl \
+    https://gist.githubusercontent.com/alexshd/756459d0e840c02d47ee/raw/d6235761fb360bad93db6017f3fedbd65a871ab1/.vimrc \
+        > ~/.vimrc
+    vim +PluginInstall +qall  2>&1
 
-  return 0
+    return 0
 }
 
 install_zsh() {
-  # Script for base install
-  sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    # Script for base install
+    sh -c \
+    "$(curl -fsSL \
+        https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 }
 
 config_zsh() {
@@ -49,17 +54,17 @@ export LANG=en_US.UTF-8
 export EDITOR=vim
 EOF
 
-  return 0
+    return 0
 }
 
 main() {
-  ubuntu_update
-  base_packages
-  install_chef
-  install_zsh
-  config_zsh
-  setup_vim
-  return 0
+    ubuntu_update
+    base_packages
+    install_chef
+    install_zsh
+    config_zsh
+    setup_vim
+    return 0
 }
 
 main
